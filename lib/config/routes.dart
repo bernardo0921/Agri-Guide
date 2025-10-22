9c059c52e6dc3b94221a32aabed52c4d4375588a
bernardo0921 <ephraimbernard7@gmail.com>
2025-10-22 23:55:50 +0000

    added language screen to the routes

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import '../screens/splash_screen.dart';
 import '../screens/home/home_screen.dart';
+import '../screens/language_screen.dart';
 
 class AppRoutes {
   // Route names
   static const String splash = '/';
+  static const String language = '/language';
   static const String home = '/home';
   static const String marketplace = '/marketplace';
   static const String profile = '/profile';
@@ -15,6 +17,7 @@ class AppRoutes {
   static Map<String, WidgetBuilder> getRoutes() {
     return {
       splash: (context) => const SplashScreen(),
+      language: (context) => const LanguageScreen(),
       home: (context) => const HomeScreen(),
       // marketplace: (context) => const MarketplaceScreen(),
       // profile: (context) => const ProfileScreen(),
